486d3a48d6a84ae1700c8788daa76939db818955
pyypel <129758896+pyypel@users.noreply.github.com>
2025-10-15 08:00:59 +0300

    1 loeng

---
+++ code/01/app.js
@@ -0,0 +1,65 @@
+//alert("Tere jälle");
+//confirm("Kas oled kindel?");
+//let firstName = 'Pyy';
+//firstName = "Püü";
+//alert(firstName);
+
+//firstName = prompt("Sisesta oma nimi");
+//alert("Tere, " + firstName);
+
+//let choice = confirm("Vali ei või jah");
+//alert(choice);
+
+
+//a=prompt("Sisesta esimene arv");
+//b=prompt("Sisesta teine arv");
+//alert(Number(a)+Number(b));
+//alert(a-b)
+
+//age= 30;
+//alert(age)
+//alert(age+10)
+
+psw = prompt("Sisesta parool");
+if (psw === 123) {
+    alert("Õige");
+} else {
+    alert("Vale");
+}   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
